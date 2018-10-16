2c7793f6041a1a9a7a42dae8151b74afbc730f88
Dana Luther <dluther@envisageinternational.com>
2018-10-16 16:23:23 -0700

    Init for ZCOE2018

---
+++ public_html/command-line.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * command-line.php
+ *
+ * @author danaluther  <dluther@envisageinternational.com>
+ * @copyright (c) Envisage International, Corp
+ * @created 10/16/18 12:15 PM
+ */
+echo PHP_EOL.PHP_EOL.'Hello ZendCon & OpenEnterprise 2018!'.PHP_EOL.PHP_EOL;
---
+++ public_html/index.php
@@ -0,0 +1,34 @@
+<?php
+error_reporting( E_NONE );
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Hello ZCOE</title>
+</head>
+<body>
+<div class="jumbotron text-center">
+    <h1>Hello, ZCOE18!</h1>
+    <img src="/img/horizontal.png"/> <br/>
+    PHP Version: <?= phpversion(); ?><br/>
+	<?php
+	if ( extension_loaded( 'mysqli' ) )
+	{
+		$db = new \mysqli( 'db', 'root', 'sunshinephp' );
+		echo "MySQL Enabled.";
+	} else
+	{
+		echo '<span class="text-danger">MySQL extension is not currently enabled</span>';
+
+	}
+	?>
+</div>
+
+<div class="text-center">
+    <a href="phpinfo.php" class="btn btn-lg btn-primary">Click here to check the php settings</a>
+</div>
+</body>
+</html>
